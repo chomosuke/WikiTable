75f4f1c871e1cae17645e32991bac662f3e8c0a3
chomosuke <a13323600@gmail.com>
2021-09-12 13:23:54 +1000

    matched spec kinda

--- wiki_table/__main__.py
+++ wiki_table/__main__.py
@@ -1,3 +1,14 @@
 # the cmd interface will be here.
-import filter_num_columns
-import html_to_columns
+from filter_num_columns import filter_num_columns
+from html_to_columns import html_to_columns
+import urllib.request
+import matplotlib.pyplot as plt
+
+url = input('pls gib wikipedia url: ')
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    columns = html_to_columns(html)
+    num_columns = filter_num_columns(columns)
+    plt.bar(range(len(num_columns[0].content)), num_columns[0].content)
+    plt.ylabel(num_columns[0].name)
+    plt.savefig('plt')
